a9301d011847ddf4fec835cefa9e4b9c30a5eb1e
Damian <damiandreczewski@gmail.com>
2024-01-28 18:08:07 +0100

    kalkulator

--- calc.py
+++ calc.py
@@ -1,1 +1,24 @@
 # kalkulator
+
+import sys
+
+if len(sys.argv) !=4:
+    print("Użycie: calc.py <num1> < + - > <num2>")
+    sys.exit(1)
+
+num1 = int(sys.argv[1])
+op = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    eesult = num1 - num2
+else:
+    print("Zły operator")
+    sys.exit(1)
+
+with open("/tmp/wynik.txt", "w") as file:
+    file.write( str(result) )
+
+print( result )
